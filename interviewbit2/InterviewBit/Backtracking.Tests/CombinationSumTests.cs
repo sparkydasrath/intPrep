edae8f75003320a9f098c92a9f921487d6691010
sparkydasrath <sdasrath@gmail.com>
2019-03-03 15:19:35 -0500

    more

---
+++ interviewbit2/InterviewBit/Backtracking.Tests/CombinationSumTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Backtracking.Tests
+{
+    [TestFixture]
+    public class CombinationSumTests
+    {
+        [Test]
+        public void ShouldGetList()
+        {
+            CombinationSum cs = new CombinationSum();
+            System.Collections.Generic.List<System.Collections.Generic.List<int>> r = cs.CombinationSumImp(new int[] { 2, 3, 6, 7 }, 7);
+            Assert.That(r.Count, Is.EqualTo(2));
+        }
+    }
+}
---
+++ interviewbit2/InterviewBit/Backtracking/CombinationSum.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Backtracking
+{
+    public class CombinationSum
+    {
+        private List<List<int>> results = new List<List<int>>();
+
+        public List<List<int>> CombinationSumImp(int[] candidates, int target)
+        {
+            CombinationSumHelper(candidates, target, new List<int>(), 0);
+            return results;
+        }
+
+        private void CombinationSumHelper(int[] candidates, int target, List<int> accumulator, int start)
+        {
+            if (accumulator.Sum() == target)
+            {
+                results.Add(accumulator);
+                return;
+                ;
+            }
+
+            for (int i = start; i < candidates.Length; i++)
+            {
+                accumulator.Add(candidates[i]);
+
+                if (accumulator.Sum() <= target)
+                    CombinationSumHelper(candidates, target, new List<int>(accumulator), i);
+
+                accumulator.RemoveAt(accumulator.Count - 1);
+            }
+        }
+
+        private void CombinationSumHelper2(int[] candidates, int target, List<int> accumulator, int start)
+        {
+            //combo sum 2
+            // see LC
+            if (accumulator.Sum() == target)
+            {
+                results.Add(accumulator);
+                return;
+            }
+
+            for (int i = start; i < candidates.Length; i++)
+            {
+                accumulator.Add(candidates[i]);
+
+                if (accumulator.Sum() <= target)
+                    CombinationSumHelper(candidates, target, new List<int>(accumulator), i + 1);
+
+                accumulator.RemoveAt(accumulator.Count - 1);
+            }
+        }
+
+        private void CombinationSumHelper3(int[] candidates, int target, List<int> accumulator, int start, int k)
+        {
+            //combo sum 2
+            // see LC
+            if (accumulator.Sum() == target && accumulator.Count == k)
+            {
+                results.Add(accumulator);
+                return;
+            }
+
+            for (int i = start; i < candidates.Length; i++)
+            {
+                accumulator.Add(candidates[i]);
+
+                if (accumulator.Sum() <= target && accumulator.Count <= k)
+                    CombinationSumHelper(candidates, target, new List<int>(accumulator), i + 1);
+
+                accumulator.RemoveAt(accumulator.Count - 1);
+            }
+        }
+    }
+}
