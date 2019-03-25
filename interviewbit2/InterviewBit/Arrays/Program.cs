f409e3d3cd753b2b214b1296503b4899f4fa175e
sparky.dasrath <sdasrath@gmail.com>
2019-03-25 14:14:57 -0400

    centerbridge i think

--- interviewbit2/InterviewBit/Arrays/Program.cs
+++ interviewbit2/InterviewBit/Arrays/Program.cs
@@ -1,31 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Arrays
 {
+    internal interface IMine
+    {
+        event Action MyEnvent;
+
+        int MyProp { get; set; }
+    }
+
+    public class X
+    {
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+    }
+
     internal class Program
     {
-        public static bool IsPowerOfTwo(int n)
+        public static int findParent(int processNumber)
         {
-            while (n > 1 && n % 2 == 0)
+            /*
+* Complete the 'findParent' function below.
+*
+* The function is expected to return an INTEGER.
+* The function accepts INTEGER processNumber as parameter.
+*/
+        }
+
+        public static string FirstRepeatedWord(string s)
+        {
+            HashSet<string> words = new HashSet<string>();
+
+            char[] delim = { ' ', '\t', ',', ':', ';', '-', '.' };
+            string[] splitWords = s.Split(delim);
+
+            for (int i = 0; i < splitWords.Length; i++)
             {
-                n /= 2;
+                if (!words.Contains(splitWords[i]))
+                    words.Add(splitWords[i]);
+                else return splitWords[i];
             }
 
-            return n == 1;
+            return null;
+        }
+
+        public static IEnumerable<int> Power(int n, int e)
+        {
+            int res = 1;
+            for (int i = 0; i < e; i++)
+            {
+                res = res * n;
+                yield return res;
+            }
         }
 
         private static void Main(string[] args)
         {
-            List<int> l1 = new List<int> { 1, 6 };
-            List<int> l2 = new List<int> { 2, 8 };
-            List<int> l3 = new List<int> { 7, 12 };
+            string s = FirstRepeatedWord("He had had quite enough of this nonsense.");
 
-            List<int> a = l1.Union(l2).ToList();
-            List<int> b = a.Union(l3).ToList();
+            HashSet<string> ss = new HashSet<string>
+            {
+                "had",
+                "Had"
+            };
 
+            Console.WriteLine(ss.Contains("had"));
+            Console.WriteLine(ss.Contains("Had"));
+
+            Console.WriteLine(s);
             Console.ReadLine();
         }
+
+        private static int Minn(int x = 2, int y = 4, int z = 3) => Math.Min(x, Math.Min(y, z));
     }
 }
