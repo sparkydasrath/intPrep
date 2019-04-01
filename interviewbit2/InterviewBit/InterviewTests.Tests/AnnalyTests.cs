19ba09bd76fd7402518c1dc125cc342f1ecd87df
Sparky Dasrath <sdasrath@gmail.com>
2019-04-01 17:22:41 -0400

    annaly hackerrank test

---
+++ interviewbit2/InterviewBit/InterviewTests.Tests/AnnalyTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace InterviewTests.Tests
+{
+    [TestFixture]
+    public class AnnalyTests
+    {
+        [TestCase(new float[] { 1.50f, 1.50f, 1.50f, 1.50f, 1.50f }, ExpectedResult = 3)]
+        public int EfficientJanitor(float[] weights)
+        {
+            int result = Annaly.EfficientJanitorHelper(weights);
+            return result;
+        }
+
+        [TestCase(new int[] { 7, 2, 3, 10, 2, 4, 8, 1 }, ExpectedResult = 8)]
+        [TestCase(new int[] { 5, 10, 8, 7, 6, 5 }, ExpectedResult = 5)]
+        [TestCase(new int[] { 6, 7, 9, 5, 6, 3, 2 }, ExpectedResult = 2)]
+        public int MaxDifference(int[] nums)
+        {
+            int result = Annaly.MaxDifferenceInner(nums);
+            return result;
+        }
+
+        [TestCase(new int[] { 3, 1, 2, 3 }, new int[] { 3, 2, 1, 3 }, "Even", ExpectedResult = "Andrea")]
+        public string Winner(int[] a, int[] m, string s)
+        {
+            string result = Annaly.WinnerHelper(a, m, s);
+            return result;
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
---
+++ interviewbit2/InterviewBit/InterviewTests/Annaly.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InterviewTests
+{
+    public class Annaly
+    {
+        public static int EfficientJanitor(List<float> weight)
+        {
+            if (weight == null || weight.Count == 0) return 0;
+
+            int trips = 0;
+            for (int i = 0; i < weight.Count; i++)
+            {
+                if (weight[i] == float.MinValue) continue;
+                for (int j = i + 1; j < weight.Count; j++)
+                {
+                    // find the first pair where the sum < 3.00
+                    if (weight[i] == float.MinValue || weight[j] == float.MinValue) continue;
+
+                    float sum = weight[i] + weight[j];
+                    if (sum <= 3.00f)
+                    {
+                        trips += 1;
+                        weight[i] = float.MinValue;
+                        weight[j] = float.MinValue;
+                    }
+                }
+            }
+
+            // check if there are any left over weights left
+            IEnumerable<float> leftovers = weight.Where(w => w != float.MinValue);
+            trips += leftovers.Count();
+            return trips;
+        }
+
+        public static int EfficientJanitorHelper(float[] weight) => EfficientJanitor(weight.ToList());
+
+        public static int MaxDifference(List<int> nums)
+        {
+            if (nums == null || nums.Count == 0) return 0;
+
+            int max = 0;
+            for (int i = 1; i < nums.Count; i++)
+            {
+                List<int> temp = nums.GetRange(0, i);
+                int currentNum = nums[i];
+                int tempMax = 0;
+                for (int j = 0; j < temp.Count; j++)
+                    temp[j] = currentNum - temp[j];
+
+                Console.WriteLine($"temp pass on current {currentNum}");
+                foreach (int x in temp)
+                {
+                    Console.WriteLine(string.Join(" ", x));
+                }
+
+                tempMax = temp.Max();
+
+                max = Math.Max(max, tempMax);
+            }
+
+            return max;
+        }
+
+        public static int MaxDifferenceInner(int[] nums) => MaxDifference(nums.ToList());
+
+        public static string Winner(List<int> andrea, List<int> maria, string s) => WinnerHelper(andrea.ToArray(), maria.ToArray(), s);
+
+        public static string WinnerHelper(int[] a, int[] m, string s)
+        {
+            List<int> andrea = a.ToList();
+            List<int> maria = m.ToList();
+
+            if (andrea == null && maria == null) return "Tie";
+            if (andrea == null && maria != null) return "Maria";
+            if (andrea != null && maria == null) return "Andrea";
+
+            List<int> andreaResults = new List<int>();
+            List<int> mariaResults = new List<int>();
+
+            int cardCount = andrea.Count;
+
+            if (s == "Odd")
+            {
+                andrea.RemoveAt(0);
+                maria.RemoveAt(0);
+            }
+
+            for (int i = 0; i < cardCount; i++)
+            {
+                if (i % 2 == 0 || i % 2 == 0)
+                {
+                    andreaResults.Add(andrea[i] - maria[i]);
+                    mariaResults.Add(maria[i] - andrea[i]);
+                }
+            }
+
+            int andreaScore = andreaResults.Sum();
+            int mariaScore = mariaResults.Sum();
+
+            if (andreaScore > mariaScore) return "Andrea";
+            if (mariaScore > andreaScore) return "Marie";
+            return "Tie";
+        }
+    }
+}
