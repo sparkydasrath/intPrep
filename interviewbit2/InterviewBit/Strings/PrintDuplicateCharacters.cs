0d6b5dfbb21e2a669eaffe3d422f7077f4993079
Sparky Dasrath <sdasrath@gmail.com>
2019-04-07 15:47:15 -0400

    Some prep for that boa role

---
+++ interviewbit2/InterviewBit/Strings/PrintDuplicateCharacters.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Strings
+{
+    public class PrintDuplicateCharacters
+    {
+        public string GetDuplicates(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s)) return string.Empty;
+
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            foreach (char c in s)
+            {
+                if (!dictionary.ContainsKey(c)) dictionary[c] = 1;
+                else dictionary[c]++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (KeyValuePair<char, int> kvp in dictionary)
+                if (kvp.Value > 1) sb.Append(kvp.Key);
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ interviewbit2/InterviewBit/StringsTests/PrintDuplicateCharactersTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Strings.Tests
+{
+    [TestFixture]
+    public class PrintDuplicateCharactersTests
+    {
+        [TestCase(null, ExpectedResult = "")]
+        [TestCase("a", ExpectedResult = "")]
+        [TestCase("aabc", ExpectedResult = "a")]
+        [TestCase("aabbbcdeee", ExpectedResult = "abe")]
+        public string GetDuplicated(string s)
+        {
+            PrintDuplicateCharacters pdc = new PrintDuplicateCharacters();
+            string results = pdc.GetDuplicates(s);
+            return results;
+        }
+    }
+}
