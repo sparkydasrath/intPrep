f1357ad18a094c652423e63aa9c777697cb48f6a
Sparky Dasrath <sdasrath@gmail.com>
2019-05-05 00:45:12 -0400

    blackstone prep
    - CashRegister seems to work

--- interviewbit2/InterviewBit/InterviewTests.Tests/Blackstone/CashRegisterTests.cs
+++ interviewbit2/InterviewBit/InterviewTests.Tests/Blackstone/CashRegisterTests.cs
@@ -10,7 +10,7 @@ namespace InterviewTests.Tests.Blackstone
         public void ShouldReturnCorrectChange()
         {
             CashRegister cr = new CashRegister();
-            string result = cr.GetChange();
+            string result = cr.GetChange("15.94;16.00");
             Assert.That(result, Is.EqualTo("NICKEL,PENNY"));
         }
     }
--- interviewbit2/InterviewBit/InterviewTests/Blackstone/CashRegister.cs
+++ interviewbit2/InterviewBit/InterviewTests/Blackstone/CashRegister.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace InterviewTests.Blackstone
 {
@@ -16,34 +18,68 @@ namespace InterviewTests.Blackstone
 
             if (priceCashPair.CashGiven < priceCashPair.PurchasePrice) return Error;
 
-            Dictionary<double, string> denominationMap = GetDenominations();
+            Dictionary<decimal, string> denominationMap = GetDenominations();
 
-            double transaction = priceCashPair.CashGiven - priceCashPair.PurchasePrice;
+            decimal transaction = Math.Round(priceCashPair.CashGiven - priceCashPair.PurchasePrice, 2, MidpointRounding.AwayFromZero);
 
-            if (Math.Abs(transaction) < 0.001) return denominationMap[transaction];
+            if (transaction == 0) return Zero;// denominationMap[transaction];
 
-            return null;
+            var keys = denominationMap.Keys.ToList();
+            List<string> results = new List<string>();
+
+            for (int i = keys.Count - 1; i >= 0; i--)
+            {
+                // work backwards trying to use each denomination
+                decimal div = transaction / keys[i];
+                decimal rem = transaction % keys[i];
+
+                // if you can't use the denomination, just continue
+                // ex: you have a change of $5 but when you do $5/100, you get zero, so you know you
+                // can't give $100 back in change
+
+                if (IsDivisionEqualToZero(div)) continue;
+
+                // otherwise, if we can use this denomination, the value of div will tell you how%
+                // many times you can use it
+                // ex: if transaction = $13 then we do 16/10 = 1, so you have at least one TEN in the result
+                // now the rest of the transaction to process = 16 % 10 ==> 6
+
+                // append the result to the sb and update the div
+                for (int j = 0; j < Math.Floor(div); j++)
+                {
+                    // get the denomination
+                    decimal denomination = keys[i];
+                    transaction -= denomination;
+                    results.Add(denominationMap[denomination]);
+                }
+            }
+
+            return string.Join(",", results);
         }
 
-        private Dictionary<double, string> GetDenominations()
+        private Dictionary<decimal, string> GetDenominations()
         {
-            Dictionary<double, string> denominationMap = new Dictionary<double, string>(12)
+            Dictionary<decimal, string> denominationMap = new Dictionary<decimal, string>(11)
             {
-                [0.00d] = "ZERO",
-                [0.01d] = "PENNY",
-                [0.05d] = "NICKEL",
-                [0.10d] = "DIME",
-                [0.25d] = "QUARTER",
-                [0.50d] = "HALF DOLLAR",
-                [1.00d] = "ONE",
-                [2.00d] = "TWO",
-                [5.00d] = "FIVE",
-                [10.00d] = "TEN",
-                [20.00d] = "TWENTY",
-                [100.00d] = "ONE HUNDRED",
+                [0.01m] = "PENNY",
+                [0.05m] = "NICKEL",
+                [0.10m] = "DIME",
+                [0.25m] = "QUARTER",
+                [0.50m] = "HALF DOLLAR",
+                [1.00m] = "ONE",
+                [2.00m] = "TWO",
+                [5.00m] = "FIVE",
+                [10.00m] = "TEN",
+                [20.00m] = "TWENTY",
+                [100.00m] = "ONE HUNDRED",
             };
-
             return denominationMap;
+        }
+
+        private bool IsDivisionEqualToZero(decimal division)
+        {
+            string[] divAsString = division.ToString(CultureInfo.InvariantCulture).Split('.');
+            return Convert.ToInt32(divAsString[0]) == 0;
         }
 
         private PriceCashPair ParseInput(string input)
@@ -51,15 +87,15 @@ namespace InterviewTests.Blackstone
             var split = input.Split(';');
             return new PriceCashPair
             {
-                PurchasePrice = Convert.ToDouble(split[0]),
-                CashGiven = Convert.ToDouble(split[1])
+                PurchasePrice = Convert.ToDecimal(split[0]),
+                CashGiven = Convert.ToDecimal(split[1])
             };
         }
 
         private struct PriceCashPair
         {
-            public double CashGiven { get; set; }
-            public double PurchasePrice { get; set; }
+            public decimal CashGiven { get; set; }
+            public decimal PurchasePrice { get; set; }
         }
     }
 }
