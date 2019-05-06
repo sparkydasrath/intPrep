b09788d10af423a467e5c1f97f2ea00f471bd3d5
Sparky Dasrath <sdasrath@gmail.com>
2019-05-05 22:12:12 -0400

    Pivotal Labs
    - built a crappy Set using TDD to drive the design

---
+++ interviewbit2/InterviewBit/InterviewTests.Tests/Pivotal/MySetTests.cs
@@ -0,0 +1,42 @@
+﻿using InterviewTests.Pivotal;
+using NUnit.Framework;
+
+namespace InterviewTests.Tests.Pivotal
+{
+    [TestFixture]
+    public class MySetTests
+    {
+        [Test(Description = "Should be able to Add a key to the set")]
+        public void ShouldAddItemToSet()
+        {
+            MySet<int> mySet = new MySet<int>();
+            mySet.Add(2);
+            Assert.That(mySet.Contains(2));
+        }
+
+        [Test(Description = "Should be able to remove a key if present")]
+        public void ShouldRemoveItemFromSetIfPresent()
+        {
+            MySet<int> mySet = new MySet<int>();
+            mySet.Add(3);
+            Assert.IsTrue(mySet.Contains(3));
+            mySet.Remove(3);
+            Assert.IsFalse(mySet.Contains(3));
+        }
+
+        [Test(Description = "Check if the set contains the given key")]
+        public void ShouldReturnTrueIfSetContainsKey()
+        {
+            MySet<int> mySet = new MySet<int>();
+            bool result = mySet.Contains(1);
+            Assert.IsFalse(result);
+        }
+
+        [Test(Description = "Verify that you can create a default instance of MySet")]
+        public void VerifyThatYouCanCreateANewInstanceOfMySet()
+        {
+            MySet<int> mySet = new MySet<int>();
+            Assert.That(mySet, Is.Not.Null);
+        }
+    }
+}
---
+++ interviewbit2/InterviewBit/InterviewTests/Pivotal/MySet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewTests.Pivotal
+{
+    public class MySet<T>
+    {
+        private readonly Dictionary<T, int> map;
+
+        public MySet()
+        {
+            map = new Dictionary<T, int>();
+        }
+
+        public void Add(T key)
+        {
+            if (!Contains(key))
+                map.Add(key, 0);
+        }
+
+        public bool Contains(T key)
+        {
+            return map.ContainsKey(key);
+        }
+
+        public void Remove(T key)
+        {
+            if (Contains(key))
+                map.Remove(key);
+        }
+    }
+}
