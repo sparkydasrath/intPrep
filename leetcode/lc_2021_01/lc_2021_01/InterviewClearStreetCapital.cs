ca4dcae84937c653d6449964b0bb28b761afbf24
sparky dasrath <sdasrath@gmail.com>
2021-05-09 10:21:20 -0400

    added cleatstreet interview

---
+++ leetcode/lc_2021_01/lc_2021_01/InterviewClearStreetCapital.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lc_2021_01
+{
+    public class InterviewClearStreetCapital
+    {
+        // tests from codesignal.com
+
+        public static int BuyAndSellStock(int[] prices)
+        {
+            // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/204181/C
+            if (prices.Length == 0) return 0;
+            int minProfit = int.MaxValue;
+            int maxProfit = int.MinValue;
+            foreach (int p in prices)
+            {
+                minProfit = Math.Min(minProfit, p);
+                maxProfit = Math.Max(maxProfit, p - minProfit);
+            }
+            
+            return maxProfit;
+
+        }
+    }
+}
--- leetcode/lc_2021_01/lc_2021_01/Program.cs
+++ leetcode/lc_2021_01/lc_2021_01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Net.Http.Headers;
 using Console = System.Console;
 
 namespace lc_2021_01
@@ -36,20 +37,25 @@ namespace lc_2021_01
 
             // LcEasyRunner.RunClimbStairs();
 
-       
+
             // LcEasyRunner.RunRomanToInt();
 
 
 
             //=======================================================
             // !MEDIUM RUNNER
-            
+
             // LcMediumRunner.RunLetterCombinations();
 
             // LcMediumRunner.RunCombine();
 
-       
-     
+
+
+            Console.WriteLine(InterviewClearStreetCapital.BuyAndSellStock(new[] { 7, 1, 5, 3, 6, 4 })  + "Expected 5");
+            Console.WriteLine(InterviewClearStreetCapital.BuyAndSellStock(new[] { 6, 3, 1, 2, 5, 4 }) + " Expected 4");
+            Console.WriteLine(InterviewClearStreetCapital.BuyAndSellStock(new[] { 8,5,3,1 }) + " Expected 0");
+            Console.WriteLine(InterviewClearStreetCapital.BuyAndSellStock(new[] { 3, 100, 1, 97 }) + " Expected 97");
+
 
             Console.ReadLine();
         }
